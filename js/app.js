dd974bfbe3fe3335f6c108a5e45ef95528d0aa4b
maadcode <manueldavila.developer@gmail.com>
2021-01-15 17:19:15 -0500

    Adding read viewport in javascript

--- js/app.js
+++ js/app.js
@@ -1,3 +1,14 @@
+const DATA = {
+    mobile: {
+        "woman": "./images/illustration-woman-online-mobile.svg",
+        "bg": "./images/bg-pattern-mobile.svg"
+    },
+    desktop: {
+        "woman": "./images/illustration-woman-online-desktop.svg",
+        "bg": "./images/bg-pattern-desktop.svg"
+    }
+}
+
 class Accordion {
     constructor(selector, multiple) {
         this.accordion = document.querySelector(selector)
@@ -24,9 +35,36 @@ class Accordion {
         })
     }
 }
-  
-// If you want enable multiple options, you must change the second parameter to true :D
-  
-(function() {
-    new Accordion('.accordion', false)
-})()
+
+window.addEventListener('DOMContentLoaded', () => {
+    const VIEWPORT_WIDTH = window.screen.width
+    let container = document.querySelector('.container__img')
+
+    if(VIEWPORT_WIDTH >= 820) {
+        let data = DATA.desktop
+        let imgWoman = document.createElement('img')
+        imgWoman.classList.add('woman2')
+        imgWoman.src = data.woman
+        container.appendChild(imgWoman)
+        let imgBg = document.createElement('img')
+        imgBg.classList.add('pattern2')
+        imgBg.src = data.bg
+        container.appendChild(imgBg)
+    } else {
+        let data = DATA.mobile
+        let imgWoman = document.createElement('img')
+        imgWoman.classList.add('woman')
+        imgWoman.src = data.woman
+        container.appendChild(imgWoman)
+        let imgBg = document.createElement('img')
+        imgBg.classList.add('pattern')
+        imgBg.src = data.bg
+        container.appendChild(imgBg)
+    }
+       
+    (function() {
+        new Accordion('.accordion', false)
+    })()
+})
+
+
